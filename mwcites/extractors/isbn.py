9c7feb07f0db3647f41131810e1dded620114105
Kamil Nowak <qamilnowak@gmail.com>
2018-05-20 17:01:47 +0200

    Update isbn.py

--- mwcites/extractors/isbn.py
+++ mwcites/extractors/isbn.py
@@ -1,8 +1,15 @@
 import re
 from ..identifier import Identifier
 
-ISBN_RE = re.compile('isbn\s?=?\s?([0-9\-Xx]+)', re.I)
+# Also correctly parses malformed inputs such as below:
+# isbn=2 906700-09-6 (notice the space instead of a hyphen) or
+# isbn=2 10 004179 7 (notice spaces instead of hyphens)
+ISBN_RE = re.compile('isbn\s?=?\s?([\d]+([\d\s\-]+)[\dXx])', re.I)
+
 
 def extract(text):
     for match in ISBN_RE.finditer(text):
-        yield Identifier('isbn', match.group(1).replace('-', ''))
+        yield Identifier(
+            'isbn',
+            match.group(1).replace('-', '').replace(' ', '').strip()
+        )
