4f5c3539f7cd81519cf45c5951949487d56c1091
Xarvalus <GromadzkiMarcin@gmail.com>
2018-05-23 16:54:58 +0200

    ISBN update, pipe regex fix

--- mwcites/extractors/isbn.py
+++ mwcites/extractors/isbn.py
@@ -4,7 +4,7 @@ from ..identifier import Identifier
 # Also correctly parses malformed inputs such as below:
 # isbn=2 906700-09-6 (notice the space instead of a hyphen) or
 # isbn=2 10 004179 7 (notice spaces instead of hyphens)
-ISBN_RE = re.compile('isbn\s?=?\s?([\d]+([\d\s\-]+)[\dXx])', re.I)
+ISBN_RE = re.compile('isbn\s?[=|\|]?\s?([\d]+([\d\s\-]+)[\dXx])', re.I)
 
 
 def extract(text):
